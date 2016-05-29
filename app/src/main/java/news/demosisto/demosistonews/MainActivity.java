1e6638a22072b9cc6a14a273b482e6065dd41b7c
Len Chan <mail@lenchan139.org>
2016-05-29 19:12:15 +0800

    ADD_FUNCTION: Search something by category

--- app/src/main/java/news/demosisto/demosistonews/MainActivity.java
+++ app/src/main/java/news/demosisto/demosistonews/MainActivity.java
@@ -10,21 +10,26 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private EditText txtEdit;
     public SharedPreferences dataSet;
+    private Spinner catSpinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +43,10 @@ public class MainActivity extends AppCompatActivity {
         dataSetEditor.commit();
         Log.v("pref_first_push",dataSet.getString(getString(R.string.pref_str),"Error!"));
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        catSpinner = (Spinner) findViewById(R.id.catSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.catIdArray, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        catSpinner.setAdapter(adapter);
 
         setSupportActionBar(toolbar);
         if (savedInstanceState == null) {
@@ -49,27 +58,16 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 Snackbar.make(view, "Fetching......", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-                String forCompare = txtEdit.getText().toString();
-                forCompare.trim();
-                txtEdit.setText(forCompare);
-                SharedPreferences.Editor dataSetEditor = dataSet.edit();
-                if(forCompare.indexOf("=") >= 0){
-                    dataSetEditor.putString(getString(R.string.pref_str),getResources().getString(R.string.url) + "/?" + forCompare);
-                    dataSetEditor.commit();
-                    addRssFragment();
-                }else{
-                    dataSetEditor.putString(getString(R.string.pref_str),getResources().getString(R.string.url)
-                            +getString(R.string.feed_header) + forCompare);
-                    dataSetEditor.commit();
-                    addRssFragment();
-                }
-                txtEdit.setText("");
+                doSearch();
+
                 InputMethodManager ime = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
                 ime.hideSoftInputFromWindow(view.getWindowToken(),0);
             }
         });
+
 
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
     }
@@ -109,5 +107,58 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void doSearch(){
+
+        String forCompare = txtEdit.getText().toString();
+        forCompare.trim();
+        txtEdit.setText(forCompare);
+        SharedPreferences.Editor dataSetEditor = dataSet.edit();
+
+
+        String srcURL = getResources().getString(R.string.url);
+
+        //convert catName to catPath
+        int intCatId = catSpinner.getSelectedItemPosition();
+
+        String catId = catNameToIdConverter(intCatId);
+
+        Log.v("catId_Before_Converted", "catId: " + catId);
+        if(catId == "none") {
+        }else{
+            srcURL = srcURL + "/category/" + catId + "/feed";
+        }
+
+        //final push
+        if(forCompare.indexOf("=") >= 0){
+            dataSetEditor.putString(getString(R.string.pref_str),srcURL + "/?" + forCompare);
+            dataSetEditor.commit();
+            addRssFragment();
+        }else{
+            dataSetEditor.putString(getString(R.string.pref_str),srcURL
+                    +getString(R.string.feed_header) + forCompare);
+            dataSetEditor.commit();
+            addRssFragment();
+        }
+        txtEdit.setText("");
+
+    }
+
+
+    public String catNameToIdConverter(int intIn){
+        String result = "none";
+        if(intIn == 0){result ="none";}
+        if (intIn == 1){result = "local";}
+        if (intIn == 2){result = "global";}
+        if (intIn == 3){result = "acgn";}
+        if (intIn == 4){result = "tech";}
+        if (intIn == 5){result = "lifestyle";}
+        if (intIn == 6){result =  "funny";}
+
+        return result;
+
+
+
     }
 }
